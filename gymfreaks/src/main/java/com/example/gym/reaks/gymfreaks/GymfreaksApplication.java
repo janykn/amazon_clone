d5c3a373b26e9cd2e5a012b6fa865b79135a9f25
RaffhanKhan <sheikraffhanKhan@gmail.com>
2023-07-14 00:36:59 +0530

    added gymfreak microservice

---
+++ gymfreaks/src/main/java/com/example/gym/reaks/gymfreaks/GymfreaksApplication.java
@@ -0,0 +1,13 @@
+package com.example.gym.reaks.gymfreaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymfreaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymfreaksApplication.class, args);
+	}
+
+}
